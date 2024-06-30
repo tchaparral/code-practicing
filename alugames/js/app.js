5ca1329a7afdac8ca3aee8ef5a0032e8a465f9c7
correali <correali@gmail.com>
2024-06-30 14:18:39 -0300

    added confirmation

--- alugames/js/app.js
+++ alugames/js/app.js
@@ -1,5 +1,8 @@
+// Count rents on console
+let counter = 0;
 // Function to change the game status (rent/return)
 function alterarStatus(id) {
+
     // Get the game item by ID
     let item = document.getElementById('game-' + id);
     // Select the div containing the game image
@@ -9,15 +12,24 @@ function alterarStatus(id) {
 
     // Check the button text to determine the action
     if(button.innerText === 'Alugar') {
+        let aluga = confirm('Deseja mesmo alugar?')
         // If the text is 'Rent', change to 'Return' and add relevant classes
-        div.classList.add('dashboard__item__img--rented');
-        button.classList.add('dashboard__item__button--return');
-        button.innerText = 'Devolver';
+        if(aluga === true) {
+            div.classList.add('dashboard__item__img--rented');
+            button.classList.add('dashboard__item__button--return');
+            button.innerText = 'Devolver';
+            counter++
+            console.log(counter)
+        }
     } else {
+        let devolve = confirm('Deseja mesmo devolver?');
         // If the text is 'Return', change to 'Rent' and remove relevant classes
-        div.classList.remove('dashboard__item__img--rented');
-        button.classList.remove('dashboard__item__button--return');
-        button.innerText = 'Alugar';
+        if(devolve === true) {
+            div.classList.remove('dashboard__item__img--rented');
+            button.classList.remove('dashboard__item__button--return');
+            button.innerText = 'Alugar';
+        }
     }
+    
 
 }
